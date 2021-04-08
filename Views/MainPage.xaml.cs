615233e84dd613dcb1ec486a0c8c691b5275a8e9
jiyeong1004 <ak1157@naver.com>
2021-04-08 09:22:29 +0900

    commit

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -15,14 +15,14 @@ namespace HelloUWP.Views
             InitializeComponent();
         }
 
-        // 즉각 실행되는 게 아니라 준비될때 실행됨
+        // 즉각 실행되는 게 아니라 준비될 때 실행됨
         private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            var myDlg = new MessageDialog("안녕하");  // 컴파일 시점에 메세지 다이얼로그로 바꿔친다.
+            var myDlg = new MessageDialog("안녕하");  // 컴파일 시점에 메시지 다이얼로그로 바꿔친다.
             myDlg.Commands.Add(new UICommand("OK"));
             myDlg.Commands.Add(new UICommand("안OK"));
             myDlg.Commands.Add(new UICommand("싫음"));
-            await myDlg.ShowAsync();  /* 파일을 다 읽을때까지(준비될 때까지) 다른 작업을 할 수 있다.
+            await myDlg.ShowAsync();  /* 파일을 다 읽을 때까지(준비될 때까지) 다른 작업을 할 수 있다.
                                                             => 자원의 1%까지 끌어들일 수 있다. => 비동기식 */
         }
     }
